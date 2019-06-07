85e99faa0c216d29a8a5d0059b5d90b3bee09410
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-06-07 23:40:38 +0530

    Step 5: Separate PHP Logic From Presentation

---
+++ example04.php
@@ -0,0 +1,1 @@
+<?php $greetings = "Hello World"; ?>
---
+++ example04.view.php
@@ -0,0 +1,13 @@
+<?php require "example04.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Step 5 : Seperate PHP Logic From HTML</title>
+</head>
+<body>
+    <?php echo $greetings; ?>
+</body>
+</html>
